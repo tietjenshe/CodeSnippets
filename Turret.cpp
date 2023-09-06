7effbe9bfc930aca72aa47351255f204784280a6
Henrik Tietjens <henrik.tietjens@stud-mail.uni-wuerzburg.de>
2023-09-06 17:42:53 +0200

    Additional comments and magic numbers removed

--- Turret.cpp
+++ Turret.cpp
@@ -8,7 +8,7 @@
 namespace spacedust {
 
 	Turret::Turret(engine::guid_t entityID, engine::EntityComponentManager& entityManager,
-		engine::Transform& shipTransform, engine::Transform* parent, 
+		engine::Transform& targetTransform, engine::Transform* parent,
 		glm::vec3 position, glm::quat rotation, glm::vec3 scale)
 		:
 		// Due to lack of serialisation in the engine a large number of parameters have to be specified here
@@ -18,8 +18,8 @@ namespace spacedust {
 			std::make_shared<engine::rendering::ReflectiveMaterial>("textures/Turret-Diffuse.png"),
 			engine::rendering::Mesh::getInstance("gltf/turret.glb"))),
 		m_rigidbody(addComponent<engine::Rigidbody>(m_transform, rp3d::BodyType::STATIC)),
-		m_firingAudio(addComponent<engine::Audio>("audio/shot_enemy.mp3", 0.04f, false)),
-		m_target(shipTransform)
+		m_firingAudio(addComponent<engine::Audio>("audio/shot_enemy.mp3", m_firingVolume, false)),
+		m_target(targetTransform)
 	{
 		m_rigidbody.addBoxCollider();
 		m_rigidbody.onEnterCollisionCallback = [&](engine::Physicsbody* otherBody) { onCollisionEntered(otherBody); };
@@ -29,40 +29,48 @@ namespace spacedust {
 	}
 
 	void Turret::update() {
+		// check if the target is in range and in line of sight if yes 
 		engine::Physicsbody* raycastHit = m_rigidbody.raycast(m_transform.getWorldPosition(), m_target.m_position, m_range);
 		if (raycastHit != nullptr && raycastHit->compareTag("Player")) {
 			rotateTowardsTarget();
 			shoot();
 		}
 
+		// tick down cooldown for shooting
 		if (m_firingCooldown > 0) {
 			m_firingCooldown -= engine::GameApplication::getDeltaTime() * m_firingRate;
 		}
 	}
 
 	void Turret::rotateTowardsTarget() {
+		// calculate the target rotation
 		glm::vec3 targetDirection = m_target.m_position - m_transform.getWorldPosition();
 		glm::quat targetRotation = glm::quatLookAt(glm::normalize(-targetDirection), glm::normalize(m_transform.getUpVector()));
 
+		// calculate the angle between current and target rotation
 		float angle = glm::degrees(glm::angle(glm::normalize(m_transform.getForwardVector()), glm::normalize(targetDirection)));
 
-		float progress = m_turningRate * engine::GameApplication::getDeltaTime() / angle;
-		progress = glm::clamp(progress, 0.0f, 1.0f);
+		// calculate the percentage of how much of that angle will be covered based on the turning speed
+		float progress = m_turningSpeed * engine::GameApplication::getDeltaTime() / angle;
+		progress = glm::clamp(progress, 0.0f, 1.0f);	// avoid overshooting
 		
+		// use slerp to rotate that percentage of the difference in rotation
 		m_transform.m_rotation = glm::slerp(m_transform.m_rotation, targetRotation, progress);
 	}
 
 	void Turret::shoot() {
 		if (m_firingCooldown <= 0) {
-			m_entityManager.createEntity<Bullet>(m_firingSpeed, m_bulletColor,
+			// spawn a bullet, play the sound and reset cooldown
+			m_entityManager.createEntity<Bullet>(m_bulletSpeed, m_bulletColor,
 				m_transform.getWorldPosition() +
-				m_transform.getForwardVector() * 6.0f, m_transform.getWorldRotation());
+				m_transform.getForwardVector() * m_bulletSpawnOffset, m_transform.getWorldRotation());
 			m_firingCooldown = 1;
 			m_firingAudio.play();
 		}
 	}
 
 	void Turret::onCollisionEntered(engine::Physicsbody* otherBody) {
+		// if hit by a bullet and take damage, if hit by the player ship or no health left destroy the turret
 		if (otherBody->compareTag("Bullet")) {
 			if (--m_health <= 0) { destroyTurret(); }
 		}
--- Turret.h
+++ Turret.h
@@ -7,11 +7,19 @@
 #include "engine/components/Audio.h"
 
 namespace spacedust {
-	/// @brief The turret entity that tracks and shoots the player ship once it enters its range
+	/// @brief The turret entity that tracks and shoots the player ship once it enters its range and has line of sight
 	class Turret : public engine::Entity {
 	public:
+		/// @brief Creates a turret entity
+		/// @param entityID The id of the entity. Automatically passed during creation.
+		/// @param entityManager The manager for all entities. Automatically passed during creation.
+		/// @param shipTransform The transform the turret will be targeting
+		/// @param parent The scenegraph parent of the turret.
+		/// @param position The turrets position relative to the parent.
+		/// @param rotation The turrets rotation relative to the parent.
+		/// @param scale The turrets scale relative to the parent.
 		Turret(engine::guid_t entityID, engine::EntityComponentManager& entityManager,
-			engine::Transform& shipTransform,
+			engine::Transform& targetTransform,
 			engine::Transform* parent = nullptr,
 			glm::vec3 position = { 0, 0, 0 },
 			glm::quat rotation = glm::quat(1, 0, 0, 0),
@@ -31,17 +39,19 @@ namespace spacedust {
 		engine::Audio& m_firingAudio;
 
 		// stats
-		int m_health = 5;
+		int m_health = 5;					// the number of hits the turret can take
 
 		// targeting
-		engine::Transform& m_target;
-		float m_range = 100.0f;
-		float m_turningRate = 90.0f;
+		engine::Transform& m_target;		// the transform that gets tracked
+		float m_range = 100.0f;				// the range at which the turret can detect the target
+		float m_turningSpeed = 90.0f;		// turning speed of the turret in degree per second
 
 		// shooting
-		glm::vec4 m_bulletColor = glm::vec4(255 / 255, 90.0f / 255, 90.0f / 255, 1);
-		float m_firingSpeed = 35.0f;
-		float m_firingRate = 2.0f;
-		float m_firingCooldown = 0;
+		const glm::vec4 m_bulletColor = glm::vec4(255 / 255, 90.0f / 255, 90.0f / 255, 1);
+		const float m_firingVolume = 0.04f;	// the volume of the firing sound
+		float m_bulletSpeed = 35.0f;		// how many units per second the bullet moves
+		float m_bulletSpawnOffset = 6.0f;	// z-offset to the turret where the bullets are spawned
+		float m_firingRate = 2.0f;			// bullets per seconds
+		float m_firingCooldown = 0;			// goes from 0 to 1 with 0 meaning ready to fire
 	};
 }
